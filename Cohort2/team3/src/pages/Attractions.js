fd4c969fce0bc68617e6da26bd52ef4b6ae791f9
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-08-09 19:16:15 -0500

    styled attractions

--- Cohort2/team3/src/pages/Attractions.js
+++ Cohort2/team3/src/pages/Attractions.js
@@ -3,6 +3,13 @@ import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import AuthContext from "auth/auth-context.js";
 import tw from "twin.macro";
 
+const TitleText = tw.h1`font-bold text-xl text-white bg-primary-900 p-8`
+const AttractionText = tw.h6`py-2`
+const Container = tw.div`m-auto w-screen p-16 h-screen`
+const Card = tw.div`w-1/2 m-auto text-center shadow-xl overflow-auto rounded`
+const Content = tw.div`p-4`
+const DoneButton = tw.button`bg-primary-900 px-4 py-2 rounded text-white mt-2`
+
 export default ({ city, itineraryId }) => {
   const authCtx = useContext(AuthContext);
   const [attractionData, setAttractionData] = useState([]);
@@ -29,19 +36,29 @@ export default ({ city, itineraryId }) => {
 
   return (
     <AnimationRevealPage>
-      <h1>Attractions Around Your Destination!</h1>
+    <Container>
+    <Card>
+
+      <TitleText>Attractions Around Your Destination!</TitleText>
+      <Content>
+
       {
         attractionData.length === 0 ?
         <div>Loading Attractions...</div> :
-        attractionData.slice(0, 5).map((el, i) => {
+        attractionData.slice(0, 10).map((el, i) => {
           return (
             <div key={i}>
-              {el.properties.name}
+            <AttractionText>{el.properties.name}</AttractionText>
             </div>
           )
           
         })
+        
       }
+      <DoneButton>Done</DoneButton>
+      </Content>
+      </Card>
+      </Container>
     </AnimationRevealPage>
   )
 };
